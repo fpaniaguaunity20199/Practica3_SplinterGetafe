3a7be7b2ba747f5e324990fceea5a74d75fc79d2
Fernando Paniagua <fpaniagua.unity.2018.9@gmail.com>
2019-11-07 14:13:07 +0100

    Initial commit

---
+++ Assets/_GameObjects/Scripts/Movedor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movedor : MonoBehaviour
+{
+    public Transform origen;
+    public Transform destino;
+    public float speed;
+    private float pct = 0;
+    private Vector3 nextPosition;
+    void Update()
+    {
+        pct += Time.deltaTime * speed;
+        nextPosition = Vector3.Lerp(origen.position, destino.position, pct);
+        if (pct >= 1)
+        {
+            pct = 1;
+            speed = -speed;
+        } else if (pct <= 0)
+        {
+            pct = 0;
+            speed = -speed;
+        }
+        transform.position = nextPosition;
+    }
+}
